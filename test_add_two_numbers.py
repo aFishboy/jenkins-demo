3b1b910718298fccf749236b7f3f3c9f9e6b7b84
Fish <samuelhoward914@gmail.com>
2024-05-09 11:54:26 -0700

    updated add two nums

--- test_add_two_numbers.py
+++ test_add_two_numbers.py
@@ -1,3 +1,20 @@
-from example_code import *
-def test_add_two_numbers_default():
-    assert addTwoNumbers(2, 3) == 2
+# test_math_operations.py
+import unittest
+from example_code import add_two_numbers
+
+class TestMathOperations(unittest.TestCase):
+
+    def test_add_two_numbers(self):
+        result = add_two_numbers(3, 5)
+        self.assertEqual(result, 8, "Expected sum of 3 + 5 to be 8")
+
+    def test_add_two_numbers_negative(self):
+        result = add_two_numbers(-3, 5)
+        self.assertEqual(result, 2, "Expected sum of -3 + 5 to be 2")
+
+    def test_add_two_numbers_zero(self):
+        result = add_two_numbers(0, 0)
+        self.assertEqual(result, 0, "Expected sum of 0 + 0 to be 0")
+
+if __name__ == '__main__':
+    unittest.main()
